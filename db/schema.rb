453e770db955dee138b6d71349c3e9a1a3b48634
Walid <walidvb@gmail.com>
2015-07-12 16:23:37 +0200

    basic style of posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626221058) do
+ActiveRecord::Schema.define(version: 20150712140539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150626221058) do
     t.datetime "updated_at"
     t.boolean  "featured",   default: false
     t.integer  "studio_id"
+    t.string   "title"
   end
 
   create_table "studios", force: true do |t|
--- spec/fabricators/post_fabricator.rb
+++ spec/fabricators/post_fabricator.rb
@@ -1,3 +1,4 @@
 Fabricator(:post) do 
+	title "Some Great Post"
 	body "The post's body"
 end
