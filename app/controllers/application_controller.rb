59bf0e52d5220ce6e9c8416ecb218ffd5bb09e39
Walid <walidvb@gmail.com>
2016-06-29 10:29:11 +0300

    rm rights for locked users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :reject_locked!, if: :devise_controller?
   before_filter :load_year
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
         user = User.find_by_email(params[:mask])
       end
       if user.nil?
-        redirect_to(root_path, alert: 'This user does not exist') 
+        redirect_to(root_path, alert: 'This user does not exist')
         return
       end
       sign_out
@@ -31,19 +31,19 @@ class ApplicationController < ActionController::Base
   # Devise permitted params
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-      :email, 
-      :password, 
-      :password_confirmation) 
+      :email,
+      :password,
+      :password_confirmation)
     }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
-      :email, 
-      :password, 
+      :email,
+      :password,
       :password_confirmation,
       :current_password
-      ) 
+      )
     }
   end
-  
+
   # Redirects on successful sign in
   def after_sign_in_path_for(resource)
     if resource.studio.nil?
@@ -53,33 +53,20 @@ class ApplicationController < ActionController::Base
       studio_student_posts_path(resource.studio, resource)
     end
   end
-  
-  # Auto-sign out locked users
-  def reject_locked!
-    if current_user && current_user.locked?
-      sign_out current_user
-      user_session = nil
-      current_user = nil
-      flash[:alert] = "Your account is locked."
-      flash[:notice] = nil
-      redirect_to root_url
-    end
-  end
-  helper_method :reject_locked!
-  
+
   # Only permits admin users
   def require_admin!
     authenticate_user!
-    
+
     if current_user && !current_user.admin?
       redirect_to root_path, alert: "You must be admin to access this page"
     end
   end
   helper_method :require_admin!
-  
+
   def require_director!
     authenticate_user!
-    if current_user && !current_user.role == :student
+    if current_user && !current_user.locked? && !current_user.role == :student
       redirect_to root_path, alert: "You must be a studio director to access this page"
     end
   end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,18 @@
 module PostsHelper
 
 	def can_edit_categories?
-		user_signed_in? && current_user.admin?
+		user_signed_in? && !current_user.locked? && current_user.admin?
 	end
 
 	def can_post_to_categories?
-		user_signed_in? && (current_user.director? || current_user.super_student?)
+		user_signed_in? && !current_user.locked? && (current_user.director? || current_user.super_student?)
 	end
 
 	def can_feature_post? post
-		user_signed_in? && current_user.can_feature_post?(post)
+		user_signed_in? && !current_user.locked? && current_user.can_feature_post?(post)
 	end
 
 	def can_edit_post? post
-		user_signed_in? &&	(current_user.admin? || current_user.can_edit_post?(post))
+		user_signed_in? && !current_user.locked? &&	(current_user.admin? || current_user.can_edit_post?(post))
 	end
 end
