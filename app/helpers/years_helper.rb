11db57ed7cfb9b56e8a384aa52a685fef3289660
Walid <walidvb@gmail.com>
2016-09-19 17:20:39 -0300

    adds old blogs

--- app/helpers/years_helper.rb
+++ app/helpers/years_helper.rb
@@ -1,2 +1,107 @@
 module YearsHelper
+
+  def old_years
+    [
+      {
+        name: 'y1s1&s2 inside lausanne 2014/15',
+        url: "http://alice.epfl.ch/page-125105-fr.html"
+      },
+      {
+        name: 'y5 projets de master 2014/15',
+        url: "http://alice.epfl.ch/page-125107-fr.html",
+      },
+      {
+        name: 'y1s1&2 where are we now? 2013/14',
+        url: "http://alice.epfl.ch/page-112621-fr.html",
+      },
+      {
+        name: 'y4s3 circle line 2013/14',
+        url: "http://enacit3srv5.epfl.ch/alice2/Y4/sa2013/",
+      },
+      {
+        name: 'y5 projets de master 2013/14',
+        url: "http://alice.epfl.ch/page-112620-fr.html",
+      },
+      {
+        name: 'y1s1 mondes visibles 2012/13',
+        url: "http://alice.epfl.ch/page-96628-fr.html",
+      },
+      {
+        name: 'y1s2 airborne: non-site/earthwork 2012/13',
+        url: "http://alice.epfl.ch/page-96484-fr.html",
+      },
+      {
+        name: 'y4s1 2048: an architectural fiction 2012/13',
+        url: "http://enacit3srv5.epfl.ch/alice2/WP_MASTER/sa2012/",
+      },
+      {
+        name: 'y4s2 hyper-typologies 2012/13',
+        url: "http://alice.epfl.ch/page-97524-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2012/13',
+        url: "http://alice.epfl.ch/page-97521-fr.html",
+      },
+      {
+        name: 'y1s1 mondes visibles 2011/12',
+        url: "http://alice.epfl.ch/page-96631-fr.html",
+      },
+      {
+        name: 'y1s2 la ville entière 2011/12',
+        url: "http://alice.epfl.ch/page-96585-fr.html",
+      },
+      {
+        name: 'y3s2 uee spatial strategies 2011/12',
+        url: "http://enacit3srv5.epfl.ch/alice2/WP_UEE/",
+      },
+      {
+        name: 'y5 projets de master 2011/12',
+        url: "http://alice.epfl.ch/page-97518-fr.html",
+      },
+      {
+        name: 'y1s1 mondes visibles 2010/11',
+        url: "http://alice.epfl.ch/page-96634-fr.html",
+      },
+      {
+        name: 'y1s2 villes archipels 2010/11',
+        url: "http://alice.epfl.ch/page-96583-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2010/11',
+        url: "http://alice.epfl.ch/page-97342-fr.html",
+      },
+      {
+        name: 'y2s1 boyant forces (boyancy) 2009/10',
+        url: "http://alice.epfl.ch/page-97356-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2009/10',
+        url: "http://alice.epfl.ch/page-97348-fr.html",
+      },
+      {
+        name: 'y2s1 altitude / m2 lausanne 2008/09',
+        url: "http://alice.epfl.ch/page-97355-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2008/09',
+        url: "http://alice.epfl.ch/page-97349-fr.html",
+      },
+      {
+        name: 'y2s1 gravity / london festival of architecture pavilion 2007/08',
+        url: "http://alice.epfl.ch/page-97354-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2007/08',
+        url: "http://alice.epfl.ch/page-97350-fr.html",
+      },
+      {
+        name: 'y2s1 spheres 2006/07',
+        url: "http://alice.epfl.ch/page-97353-fr.html",
+      },
+      {
+        name: 'y5 projets de master 2006/07',
+        url: "http://alice.epfl.ch/page-97352-fr.html",
+      }
+    ]
+  end
 end
