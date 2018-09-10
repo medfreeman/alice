0d809e26fbd15bd12249ca791451b0fc41404b50
walidvb <walidvb@gmail.com>
2018-09-10 19:38:23 +0200

    prevent slashes in year name

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,34 +1,34 @@
 class Year < ActiveRecord::Base
-	has_many :users
-	has_many :students, -> {where(role: User.roles[:student])}, class_name: "User"
-	has_many :directors, -> {where(role: User.roles[:director])}, class_name: "User"
-	has_many :posts
-	has_many :studios
-	has_many :featured_posts, ->{where(featured: true)}, class_name: 'Post'
-	validates :slug, presence: true, uniqueness: true, format: { without: /[! #?]/}
-	acts_as_taggable_on :categories
+  has_many :users
+  has_many :students, -> {where(role: User.roles[:student])}, class_name: "User"
+  has_many :directors, -> {where(role: User.roles[:director])}, class_name: "User"
+  has_many :posts
+  has_many :studios
+  has_many :featured_posts, ->{where(featured: true)}, class_name: 'Post'
+  validates :slug, presence: true, uniqueness: true, format: { without: /[! #?\/=+%&"'\$\€\£]/}
+  acts_as_taggable_on :categories
 
-	has_attached_file :logo,
-		:styles => {
-			:header => "300x100>"
-		},
-		:default_url => "logo-alice.png"
-	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :logo,
+    :styles => {
+      :header => "300x100>"
+    },
+    :default_url => "logo-alice.png"
+  validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 
-	def to_param
-		slug
-	end
+  def to_param
+    slug
+  end
 
-	def archive!
-		if !archived
-			users.where.not(admin: true).each do |u|
-				u.lock!
-				u.email += ".#{self.slug}"
-				u.confirm!
-				u.save!
-			end
-			self.archived = true
-			self.save!
-		end
-	end
+  def archive!
+    if !archived
+      users.where.not(admin: true).each do |u|
+        u.lock!
+        u.email += ".#{self.slug}"
+        u.confirm
+        u.save!
+      end
+      self.archived = true
+      self.save!
+    end
+  end
 end
